24902551e936536d53b0c1dc59d1517e72b91b33
Scasillo <santiago.castillo01@usc.edu.co>
2023-11-20 13:25:12 -0500

    Cambiso carga y terreno

--- GPTIGAMEOAC/Assets/Sprites/Transicion/transiciones.cs
+++ GPTIGAMEOAC/Assets/Sprites/Transicion/transiciones.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class transiciones : MonoBehaviour
 {
     
     private Animator transicionAnim;
+    public GameObject LoadingScreen;
+    public Slider LoadingBar;
     void Start()
     {
+        LoadingScreen.SetActive(false);
         transicionAnim = GetComponent<Animator>();
+        LoadingBar.value = 0;
     }
     public void LoadScene(string scene)
     {
@@ -19,6 +24,18 @@ public class transiciones : MonoBehaviour
     }
     IEnumerator Transiciona(string scene)
     {
+        LoadingScreen.SetActive(true);
+
+        Debug.Log("Inicio del juego");
+        
+        for (float i = 0.0f; i <= 1.1f; i += 0.1f)
+        {
+            yield return new WaitForSeconds(0.3f);
+            LoadingBar.value = i;
+        }
+        
+        Debug.Log("Fin del juego");
+        
         transicionAnim.SetTrigger("Salida");
         yield return new WaitForSeconds(1);
         SceneManager.LoadScene(scene);
