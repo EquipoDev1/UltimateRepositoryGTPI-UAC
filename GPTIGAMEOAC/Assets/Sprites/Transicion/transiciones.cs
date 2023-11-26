8243e215b81b127f2fc4bf1ab0676b96936259e7
Scasillo <santiago.castillo01@usc.edu.co>
2023-11-26 12:31:58 -0500

    Confirmar

--- GPTIGAMEOAC/Assets/Sprites/Transicion/transiciones.cs
+++ GPTIGAMEOAC/Assets/Sprites/Transicion/transiciones.cs
@@ -1,46 +1,52 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
-public class transiciones : MonoBehaviour
+public class Transiciones : MonoBehaviour
 {
-    
     private Animator transicionAnim;
     public GameObject LoadingScreen;
     public Slider LoadingBar;
+    public GameObject IntroScreen;
+    public Camera CamaraMusica;
+
     void Start()
     {
+        Time.timeScale = 1;
         LoadingScreen.SetActive(false);
+        IntroScreen.SetActive(false);
         transicionAnim = GetComponent<Animator>();
         LoadingBar.value = 0;
     }
+
     public void LoadScene(string scene)
     {
-        StartCoroutine(Transiciona(scene));
-       
-        
+        StartCoroutine(PlayVideoAndTransition(scene));
     }
-    IEnumerator Transiciona(string scene)
-    {
-        LoadingScreen.SetActive(true);
 
-        Debug.Log("Inicio del juego");
-        
+    IEnumerator PlayVideoAndTransition(string scene)
+    {
+        CamaraMusica.GetComponent<AudioListener>().enabled = false;
+        IntroScreen.SetActive(true);
+        yield return new WaitForSeconds(74);
+        StartCoroutine(Transition(scene));
+    }
+
+    IEnumerator Transition(string scene)
+    {
+        IntroScreen.SetActive(false);
+        LoadingScreen.SetActive(true);
         for (float i = 0.0f; i <= 1.1f; i += 0.1f)
         {
             yield return new WaitForSeconds(0.3f);
             LoadingBar.value = i;
         }
-        
-        Debug.Log("Fin del juego");
-        
+        Debug.Log("salidaanim");
         transicionAnim.SetTrigger("Salida");
         yield return new WaitForSeconds(1);
+
         SceneManager.LoadScene(scene);
-       
     }
-
-
 }
